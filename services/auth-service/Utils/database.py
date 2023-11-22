0aacc710c4a92687d5afb66d4f44c36e7994dc77
Eoin James McMahon <mcmahoe3@tcd.ie>
2023-11-22 11:16:35 +0000

    Auth service docker

--- services/auth-service/Utils/database.py
+++ services/auth-service/Utils/database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from .models import Base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/AuthenticationService"
+DATABASE_URL = "postgresql://postgres:password@auth-service-db-1:5432/AuthenticationService"
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+Base.metadata.create_all(bind=engine)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
