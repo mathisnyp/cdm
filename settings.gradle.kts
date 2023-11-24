353752512b8bb87ba340edf4a24c288ab0d20130
Noêl Mathis <no.l.yves.patrick.mathis@hs-furtwangen.de>
2023-11-24 10:21:54 +0000

    change project structure of auth service

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,5 +20,7 @@ rootProject.name = "city-disaster-management"
 include("frontend")
 include("services")
 include("services:sample-node-service")
-include("services:auth-service")
+include("services:auth")
+include("services:auth:service")
+include("services:auth:java-api")
 include("backend-for-frontend")
